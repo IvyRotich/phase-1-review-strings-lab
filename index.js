5052d0512fe46340c0e15b3f9a1118f737419709
Ivy Rotich <ivytetich@gmail.com>
2024-03-20 18:39:55 +0300

    String Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser="Amber Desiree";
+const welcomeMessage="Welcome to Flatbook, " + currentUser+ "!";
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, + ${currentUser.toUpperCase()} +!`;
+const shortGreeting=`Welcome, ${currentUser[0]} ${currentUser.slice(6)} !`;
+
